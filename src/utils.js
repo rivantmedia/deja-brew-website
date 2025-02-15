4cf38450f88e650d858b0d9c595496618fbf14e3
Shubhamprogrammar <mauryashubham12349@gmail.com>
2025-02-15 22:31:56 +0530

    create t3 app

--- src/utils.js
+++
@@ -1,9 +0,0 @@
-export function join(...classes) {
-  return classes.join(" ");
-}
-
-export function joinIf(condition, defaultClassName, ...otherClassNames) {
-  return condition
-    ? join(defaultClassName, ...otherClassNames)
-    : defaultClassName;
-}
